a2136136050fa0fb37ecffbec3ca4673945d68c7
Abrar Hanif Riad <ahriad.cse@gmail.com>
2025-08-02 01:32:32 +0600

    in

--- ONLINE TICKET BOOKING SYSTEM/ViewModels/BusSearchResultViewModel.cs
+++ ONLINE TICKET BOOKING SYSTEM/ViewModels/BusSearchResultViewModel.cs
@@ -6,14 +6,15 @@ namespace ONLINE_TICKET_BOOKING_SYSTEM.ViewModels
 {
     public class BusSearchResultViewModel
     {
-        public string From { get; set; }
-        public string To { get; set; }
+        public string? From { get; set; }
+        public string? To { get; set; }
         public DateTime JourneyDate { get; set; }
         public DateTime? ReturnDate { get; set; }
         public string? TripType { get; set; }
-        public List<BusSchedule> AvailableBuses { get; set; }
-    public List<BusSchedule>? ReturnBuses { get; set; }
+        public List<BusSchedule> AvailableBuses { get; set; } = new List<BusSchedule>();
+        public List<BusSchedule>? ReturnBuses { get; set; }
     }
+
 
 
 }
