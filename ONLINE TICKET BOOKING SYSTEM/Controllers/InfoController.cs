5772f1e44d09f5f6bb0f97757b49d34d7e89ab04
Abrar Hanif Riad <ahriad.cse@gmail.com>
2025-09-04 18:41:18 +0600

    in

---
+++ ONLINE TICKET BOOKING SYSTEM/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ONLINE_TICKET_BOOKING_SYSTEM.Controllers
+{
+    public class InfoController : Controller
+    {
+        [HttpGet]
+        public IActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Terms()
+        {
+            return View();
+        }
+    }
+}
